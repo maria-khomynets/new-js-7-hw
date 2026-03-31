02c5236c5c4a9501dbdce70e95e0518d70b44314
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-31 14:05:22 +0300

    hw

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,24 @@
-const btn1 = document.querySelector('#magic-btn');
-const nav = document.querySelector('.item');
-console.log(nav);
+const singleBtn = document.querySelector('#single');
+
+const handleClick = () => {
+  console.log('click event listener callback');
+};
+
+singleBtn.addEventListener('click', handleClick);
+
+// ===============================================
+const multiBtn = document.querySelector('#multiple');
+
+const firstCallback = () => {
+  console.log('First callback!');
+};
+const secondCallback = () => {
+  console.log('Second callback!');
+};
+const thirdCallback = () => {
+  console.log('Third callback!');
+};
+
+multiBtn.addEventListener('click', firstCallback);
+multiBtn.addEventListener('click', secondCallback);
+multiBtn.addEventListener('click', thirdCallback);
