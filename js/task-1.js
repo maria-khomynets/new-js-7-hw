718bb7c554fd75cefd2bb43b1faeca856256c549
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-02 10:59:11 +0300

    task

--- js/task-1.js
+++ js/task-1.js
@@ -1,22 +1,22 @@
-const btn = document.querySelector('.js-click');
-const box = document.querySelector('.js-box');
-const text = document.querySelector('h1');
-btn.addEventListener('click', click);
-box.addEventListener('click', click);
-text.addEventListener('click', click1);
+// const btn = document.querySelector('.js-click');
+// const box = document.querySelector('.js-box');
+// const text = document.querySelector('h1');
+// btn.addEventListener('click', click);
+// box.addEventListener('click', click);
+// text.addEventListener('click', click1);
 
-let step = 0;
+// let step = 0;
 
-function click() {
-  step += 20;
-  console.log(step);
-  box.style.marginTop = `${step}px`;
-  box.style.marginLeft = `${step}px`;
-}
+// function click() {
+//   step += 20;
+//   console.log(step);
+//   box.style.marginTop = `${step}px`;
+//   box.style.marginLeft = `${step}px`;
+// }
 
-function click1() {
-  step += 30;
-  console.log(step);
-  text.style.marginTop = `${step}px`;
-  text.style.marginLeft = `${step}px`;
-}
+// function click1() {
+//   step += 30;
+//   console.log(step);
+//   text.style.marginTop = `${step}px`;
+//   text.style.marginLeft = `${step}px`;
+// }
---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+// const check = document.querySelector('.js-checkbox');
+// const input = document.querySelector('.js-user-name');
+// check.addEventListener('change', event => {
+//   console.log(event.target.checked);
+// });
+
+// input.addEventListener('input', inp);
+// function inp(event) {
+//   console.log(event.target);
+// }
