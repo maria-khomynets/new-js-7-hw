a5fdb71930d040550c28fa0e4241f5c3ed7e178b
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-15 12:00:17 +0300

    d

--- js/task-1.js
+++ js/task-1.js
@@ -1,115 +1,42 @@
-// const btn = document.querySelector('.js-click');
-// const box = document.querySelector('.js-box');
-// const text = document.querySelector('h1');
-// btn.addEventListener('click', click);
-// box.addEventListener('click', click);
-// text.addEventListener('click', click1);
+// 1 - отримай body елемент і виведи його в консоль;
+const body = document.querySelector('body');
+console.log(body);
+// 2 - отримай елемент id="title" і виведи його в консоль;
+const title = document.querySelector('#title');
+console.log(title);
+// 3 - отримай елемент class="list" і виведи його в консоль;
+const list = document.querySelector('.list');
+console.log(title);
+// 4 - отримай всі елементи з атрибутом data-topic і виведи їх в консоль;
+const dataTop = document.querySelectorAll('[data-topic]');
+console.log(dataTop);
+// 5 - отримай перший елемент з списку всіх елементів з атрибутом data-topic
+//  і виведи його в консоль;
+const dataTopFirst = document.querySelector('[data-topic]');
+// 6 - отримай останній елемент з списку всіх елементів з атрибутом data-topic
+//  і виведи його в консоль;
+const el = dataTop[dataTop.length - 1];
+console.log(el);
+// 7 - який елемент є сусідом для h1? Знайти і виведи його в консоль;
+const titleSibl = title.nextElementSibling;
+// 8 - по тегу h3 знайти всі заголовки та виведи їх у консоль;
+const allTitl = document.querySelectorAll('h3');
+// 9 - для кожного елмента h3 додай class="active", який змінить колір заголовка
+// на червоний колір
+allTitl.forEach(title => title.classList.add('active'));
+// 10 - знайти елемент li який має атрибут data-topic з значенням "navigation" і виведи його в консоль;
+// 11 - додай для знайденого елемента data-topic="navigation" атрибут style і зроби його backgroundColor жовтим
+// 12 - у елемента data-topic="navigation" знайди елемент р і зміни його текст на "Я змінив тут текст!".
+// 13 - створи const currentTopic = "manipulation"; після цього знайди елемент у якогоо атрибут data-topic має значення, яке зберігається у змінній currentTopic і виведи його в консоль;
+// 14 - додай до знайденого елемента атрибут style і зроби його backgroundColor блакитним;
+// 15 - знайти в документі заголовок, який має class="completed" і виведи його в консоль;
+// 16 - видали елемент li в якому знаходиться заголовок, який має class="completed"
+// 17 - після заголовка h1 (перед списком) додай новий елемент p і задай йому наступний текст: "Об'єктна модель документа (Document Object Model)"
+// 18 - додай новий елемент списку у кінець списка, його заголовок це - "Властивість innerHTML" а опис (р) - "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу". тобто, потрібно створити елемент LI потім наповнити H3 та P і готову LI закинути у кінець списку
+// 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
+// 20 - очисти список
 
-// let step = 0;
-
-// function click() {
-//   step += 20;
-//   console.log(step);
-//   box.style.marginTop = `${step}px`;
-//   box.style.marginLeft = `${step}px`;
-// }
-
-// function click1() {
-//   step += 30;
-//   console.log(step);
-//   text.style.marginTop = `${step}px`;
-//   text.style.marginLeft = `${step}px`;
-// }
-
-/**
- * Реалізуй пошук автомобілів по сайту
- * Користувач потрапляє на сайт і одразу бачить форму для пошуку
- * і картки всіх автомобілів (масив cars)
- * Користувач може ввести в форму назву Марки або Моделі авто і в
- * тегу селект обрати що він ввів Марку або Модель (https://prnt.sc/PkkZZRy_ggtT)
- * Після натискання кнопки пошуку (сабміт форми) відмалюй авто
- * які збігаються з критеріями пошуку
- */
-
-const cars = [
-  {
-    id: 1,
-    car: 'Audi',
-    type: 'A6',
-    price: 30000,
-    img: 'https://static.wixstatic.com/media/90aeac_387e937e295a4f8586d9ff9d09b60cff~mv2.jpg/v1/fill/w_520,h_338,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/90aeac_387e937e295a4f8586d9ff9d09b60cff~mv2.jpg',
-  },
-  {
-    id: 2,
-    car: 'Honda',
-    type: 'Civic',
-    price: 12000,
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCOHzdE-dK6WK7ax8NzQolTcCWA_jhJD-CRGWfqKJIJuGs8ML_-OyiDwzsdC8jOi_K10&usqp=CAU',
-  },
-  {
-    id: 3,
-    car: 'Audi',
-    type: 'Q7',
-    price: 40000,
-    img: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/2017_Audi_Q7_S_Line_Quattro_3.0_Front.jpg',
-  },
-  {
-    id: 4,
-    car: 'BMW',
-    type: '5 siries',
-    price: 9000,
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUH96e58ynLO8SXMsFTNYkJci79eAZ8CyqcZsZ8snvzz2sfLl3Ojd1BQoaWBcrMKWvSYc&usqp=CAU',
-  },
-  {
-    id: 5,
-    car: 'Honda',
-    type: 'Accord',
-    price: 20000,
-    img: 'https://upload.wikimedia.org/wikipedia/commons/7/76/2021_Honda_Accord_Sport_%28facelift%29%2C_front_11.30.21.jpg',
-  },
-  {
-    id: 6,
-    car: 'Volvo',
-    type: 'XC60',
-    price: 7000,
-    img: 'https://winnerauto.volvocarsdealer.com/storage/local/assetPreview/image/xc60-core.jpg?w=655&h=465&size=104949&signature=563ffcae2b53c8215226d075bb4314bc',
-  },
-];
-
-const form = document.querySelector('.js-form');
-const container = document.querySelector('.js-list');
-
-function createMarkup(arr) {
-  return arr
-    .map(item => {
-      return `
-      <li class="car-card" data-id="${item.id}">
-        <img class="car-image" src="${item.img}" alt="${item.type}"/>
-        <h2 class="car-title">${item.car}</h2>
-        <h3 class="car-type">${item.type}</h3>
-        <p class="car-price">${item.price} $</p>
-      </li>
-    `;
-    })
-    .join('');
-}
-
-container.style.display = 'flex';
-container.style.flexWrap = 'wrap';
-container.style.gap = '25px';
-container.insertAdjacentHTML('beforeend', createMarkup(cars));
-
-form.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  const elements = event.target.elements;
-  const queryValue = elements.query.value;
-  const optionsValue = elements.options.value;
-
-  const result = cars.filter(item =>
-    item[optionsValue].toLowerCase().includes(queryValue.toLowerCase())
-  );
-  container.innerHTML = createMarkup(result);
-}
+console.log();
+console.log();
+console.log();
+console.log();
